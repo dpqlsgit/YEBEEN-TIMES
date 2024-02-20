5f3e023097761ae8e1dc9238fb091081f849d4b2
Yebin Kim <dpqls1227@gmail.com>
2024-02-21 00:53:23 +0900

    누나 도메인으로 변경

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = `66abf55e4f434a859eeb892551dceb60`
 let news = []
 const getLatestNews = async () => {
   //내 api key로 변경
-  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`) 
+  const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`) 
   const response = await fetch(url);
   //json : 파일 형식 중 하나 (텍스트화 시키는데 객체처럼 생김)
   const data = await response.json()
