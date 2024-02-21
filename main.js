1f3e02275ecf8ab52ca5ce3b0cd24200856215b7
Yebin Kim <dpqls1227@gmail.com>
2024-02-22 01:42:51 +0900

    url 수정

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let newsList = [];
 const getLatestNews = async () => {
   //내 api key로 변경
   //배포
-  const url = new URL(`https://yebeen-times.netlify.app/top-headlines`)
+  // const url = new URL(`https://yebeen-times.netlify.app/top-headlines`)
   //실습
-  // const url = new URL(
-    // `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
+  const url = new URL(
+    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
     // `http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-  // );
+  );
   const response = await fetch(url);
   //json : 파일 형식 중 하나 (텍스트화 시키는데 객체처럼 생김)
   const data = await response.json();
