877715115718b6e694f28c91862358411cce5561
Yebin Kim <dpqls1227@gmail.com>
2024-02-23 01:06:50 +0900

    카테고리별 / 키워드별 검색

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const API_KEY = `66abf55e4f434a859eeb892551dceb60`;
 let newsList = [];
-const getLatestNews = async () => {
+const menus = document.querySelectorAll(".menus button")
+menus.forEach(menu => menu.addEventListener('click', (event) => {
+  getNewsByCategory(event)
+}))
+
+ const getLatestNews = async () => {
   //내 api key로 변경
   //배포
   const url = new URL(`https://yebeen-times.netlify.app/top-headlines`)
   //실습
   // const url = new URL(
     // `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
-    // `http://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    // `http://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
   // );
   const response = await fetch(url);
   //json : 파일 형식 중 하나 (텍스트화 시키는데 객체처럼 생김)
@@ -16,11 +21,20 @@ const getLatestNews = async () => {
   /* newsList값이 확정되어야 데이터를 쓸 수 있기 때문에 newsList값이 확정된 후 render호출 */
   newsList = data.articles;
   render();
-  console.log("ddddd", newsList);
 };
 
-const buttonName = document.querySelectorAll(".menus button")
-console.log('buttonName',buttonName)
+const getNewsByCategory = async (event) => {
+  // console.log('category', event.target.value)
+  //ui상에서는 앞글자 대문자를 유지하고 보내주는 파라미터 값은 소문자로 변경
+  const category = event.target.textContent.toLowerCase();
+  //바뀔일 없는 API_KEY는 가장 뒤에 배치 - 쿼리 정렬 규칙 같은 것
+  const url = new URL(`http://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`)
+  const response = await fetch(url);
+  const data = await response.json();
+  //render()함수 호출 전에 newsList 변수값을 변경해주어야 한다
+  newsList = data.articles;
+  render();
+} 
 
 const render = () => {
   //변수는 최대한 const를 사용하도록!
@@ -50,3 +64,16 @@ const render = () => {
 };
 
 getLatestNews();
+
+//1. 버튼들에 클릭이벤트를 주어야 함
+//2. 카테고리별 뉴스 가져오기
+//3. 그 뉴스를 보여주기
+
+const getNewsByKeyWord = async() => {
+  let keyWord = document.getElementById('search-input').value
+  const url = new URL(`http://newsapi.org/v2/top-headlines?country=us&q=${keyWord}&apiKey=${API_KEY}`)
+  const response = await fetch(url)
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+}
